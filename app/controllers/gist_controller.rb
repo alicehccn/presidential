4a4a40f7654d4dbbbc966233b8753ca44488948a
Alice Huang <alicehccn@hotmail.com>
2015-06-28 15:54:54 -0700

    Add encrypted access tokens to cookie.

--- app/controllers/gist_controller.rb
+++ app/controllers/gist_controller.rb
@@ -1,7 +1,9 @@
 class GistController < ApplicationController
 
+  require 'aes'
+
   def index
-    access_token = session[:github_access_token]
+    access_token = get_access_token_from_cookie('github')
 
     if access_token.present?
       client = Octokit::Client.new(:access_token => access_token)
@@ -11,5 +13,26 @@ class GistController < ApplicationController
     else
       redirect_to '/oauth/authorize'
     end
+  end
+
+  private 
+
+  def get_access_token_from_cookie(service)
+    access_tokens = cookies[:presidential_access_tokens]
+    access_token = nil
+
+    if access_tokens.present?
+      begin
+        unencrypted_tokens = AES.decrypt(access_tokens, ENV['PRESIDENTIAL_AES_KEY'])
+        json_tokens = JSON.parse(unencrypted_tokens)
+
+        if json_tokens.present? && json_tokens[service].present?
+          access_token = json_tokens[service]
+        end
+      rescue
+      end
+    end
+
+    access_token
   end
 end
